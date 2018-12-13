15c52f419691f74f32c410f0a075b0098260b29c
Robert Mendoza <programadordj@gmail.com>
2018-12-13 15:28:22 -0500

    antes de colocar el eloquent

---
+++ addJob.php
@@ -0,0 +1,35 @@
+<?php
+var_dump($_GET)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+    crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+    <title>Add job</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        
+        <form action="addJob.php">
+        <h1>Add Job</h1>
+        <br>
+        <label for="">Title</label>
+        <input type="text" class="form-control" name="title">
+        <br>
+        <label for="">Description</label>
+        <input type="text" class="form-control" name="description">
+        <br>
+        <button type="submit" class="btn btn-success">Save</button>
+        </form>
+    </div>
+</div>    
+</body>
+</html>
